1646d7bb860dccc0bb64989a03c60ced10f6f1fa
jenny33141 <jennylewis33@hotmail.co.uk>
2020-02-26 15:12:25 +0000

    Updates

---
+++ precache-manifest.979cb681bef4c635895e3d40c74a815d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77dda9c8ae3c1cd18feeaf4931da35a3",
+    "url": "/gfont-clone-challenge/index.html"
+  },
+  {
+    "revision": "791dfe5fc595a019131b",
+    "url": "/gfont-clone-challenge/static/css/main.2b1b77d2.chunk.css"
+  },
+  {
+    "revision": "8785dd53fd85e5ac2769",
+    "url": "/gfont-clone-challenge/static/js/2.c33bf1e9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gfont-clone-challenge/static/js/2.c33bf1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791dfe5fc595a019131b",
+    "url": "/gfont-clone-challenge/static/js/main.dea668d7.chunk.js"
+  },
+  {
+    "revision": "bfaf37823fd7c9c49668",
+    "url": "/gfont-clone-challenge/static/js/runtime-main.766beadf.js"
+  },
+  {
+    "revision": "ace77d1b968c50fbe27fee3c1860bb34",
+    "url": "/gfont-clone-challenge/static/media/plus.ace77d1b.svg"
+  }
+]);
