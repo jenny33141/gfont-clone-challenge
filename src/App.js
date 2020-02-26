b4e9718279adc1bb17f412d83eedfb7bc2bdf58b
jenny33141 <jennylewis33@hotmail.co.uk>
2020-02-26 15:48:09 +0000

    final commit

--- src/App.js
+++ src/App.js
@@ -1,137 +1,78 @@
 import React, { Component } from "react";
+
 import "./App.css";
 import Card from "./components/Card";
-import Navbar from "./components/Navbar";
 
 class App extends Component {
   state = {
     fontInfo: [
-      
       {
         fontName: "Roboto",
         fontAuthor: "Christian Robertson",
         fontClass: "roboto",
-        showButton: false,
-        showInfo: false,
-        link: 'https://fonts.googleapis.com/css?family=Roboto&display=swap',
-        fontFam: 'Roboto',
+        showButton: false
       },
       {
         fontName: "Odibee Sans",
         fontAuthor: "James Barnard",
         fontClass: "odibeeSans",
-        showButton: false,
-        showInfo: false,
-        link: 'https://fonts.googleapis.com/css?family=Odibee+Sans&display=swap',
-        fontFam: 'Odibee Sans',
+        showButton: false
       },
       {
         fontName: "Open Sans",
         fontAuthor: "Steve Matteson",
         fontClass: "openSans",
-        showButton: false,
-        showInfo: false,
-        link: 'https://fonts.googleapis.com/css?family=Open+Sans&display=swap',
-        fontFam: 'Open Sans',
+        showButton: false
       },
       {
         fontName: "Sriracha",
         fontAuthor: "Cadson Demak",
-        fontClass: "sriracha",
-        showInfo: false,
-        link: 'https://fonts.googleapis.com/css?family=Sriracha&display=swap',
-        fontFam: 'Sriracha',
+        fontClass: "sriracha"
       },
       {
         fontName: "Trade Winds",
         fontAuthor: "Slideshow",
-        fontClass: "tradeWinds",
-        showInfo: false,
-        link: 'https://fonts.googleapis.com/css?family=Trade+Winds&display=swap',
-        fontFam: 'Trade Winds',
+        fontClass: "tradeWinds"
       },
       {
         fontName: "Akronim",
         fontAuthor: "Grzegorz Klimczewski",
-        fontClass: "akronim",
-        showInfo: false,
-        link: 'https://fonts.googleapis.com/css?family=Akronim&display=swap',
-        fontFam: 'Akronim',
+        fontClass: "akronim"
       },
       {
         fontName: "Lacquer",
         fontAuthor: "Niki Polyocan",
-        fontClass: "lacquer",
-        showInfo: false,
-        link: 'https://fonts.googleapis.com/css?family=Lacquer&display=swap',
-        fontFam: 'Lacquer',
+        fontClass: "lacquer"
       },
       {
         fontName: "Shadows Into Light",
         fontAuthor: "Kimberly Geswein",
-        fontClass: "shadowsIntoLight",
-        showInfo: false,
-        link: 'https://fonts.googleapis.com/css?family=Shadows+Into+Light&display=swap',
-        fontFam: 'Shadows Into Light',
+        fontClass: "shadowsIntoLight"
       },
       {
         fontName: "Dancing Script",
         fontAuthor: "Impallari Type",
-        fontClass: "dancingScript",
-        showInfo: false,
-        link: 'https://fonts.googleapis.com/css?family=Dancing+Script&display=swap',
-        fontFam: 'Dancing Script',
+        fontClass: "dancingScript"
       },
       {
         fontName: "Amatic SC",
         fontAuthor: "Vernon Adams",
-        fontClass: "amaticSC",
-        showInfo: false,
-        link: 'https://fonts.googleapis.com/css?family=Amatic+SC&display=swap',
-        fontFam: 'Amatic SC',
+        fontClass: "amaticSC"
       },
       {
         fontName: "Bangers",
         fontAuthor: "Vernon Adams",
-        fontClass: "bangers",
-        showInfo: false,
-        link: 'https://fonts.googleapis.com/css?family=Bangers&display=swap',
-        fontFam: 'Bangers',
+        fontClass: "bangers"
       },
       {
         fontName: "Permanent Marker",
         fontAuthor: "Vernon Adams",
-        fontClass: "permanentMarker",
-        showInfo: false,
-        link: 'https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap',
-        fontFam: 'Permanent Marker',
+        fontClass: "permanentMarker"
       }
     ],
     text: "Almost before we knew it, we had left the ground.",
-    searchTerm: "",
-    temp: [],
-    newText: "",
-    count: 0
+    newText: ""
   };
-
-  componentDidMount() {
-    this.setState({ temp: this.state.fontInfo });
-  }
-
-  searchHandler = e => {
-    let arr = [...this.state.fontInfo];
-    let arr2 = arr.filter(font => {
-      return font.fontName.toLowerCase() == e.target.value.toLowerCase();
-    });
-
-    if (e.target.value == "") {
-      this.setState({ temp: this.state.fontInfo, searchTerm: "" });
-    } else {
-      this.setState({ temp: arr2, searchTerm: e.target.value });
-    }
-    
-  };
-  
   handleChange = (event, index) => {
     console.log(index);
     let newFontInfo = [...this.state.fontInfo];
@@ -150,32 +91,12 @@ class App extends Component {
     this.setState({ text: temp });
   };
 
-  infoHandler = index => {
-    let infoBox = [...this.state.fontInfo]
-    infoBox[index].showInfo = true
-    this.setState({fontInfo: infoBox})
-    console.log("hi")
-  }
-
-  cancelHandler = index => {
-    let infoBox2 = [...this.state.fontInfo]
-    infoBox2[index].showInfo = false
-    this.setState({fontInfo: infoBox2})
-    console.log("goodbye")
-  }
-
   render() {
     return (
       <div className="container">
-        <Navbar
-          color
-          searchHandler={e => this.searchHandler(e)}
-          fonts={this.state.fontInfo}
-          val={this.state.searchTerm}
-        />
-        <div className="container2">
-          {this.state.temp.map((font, index) => {
-          return <Card
+        {this.state.fontInfo.map((font, index) => {
+          return (
+            <Card
               key={index}
               fontInfo={font}
               text={this.state.text}
@@ -183,20 +104,12 @@ class App extends Component {
               handleChange={event => this.handleChange(event, index)}
               newText={this.state.newText}
               clickHandler={event => this.clickHandler(event)}
-              fontNam={this.state.fontInfo[index].fontName}
-              links={this.state.fontInfo[index].link}
-              fontFam={this.state.fontInfo[index].fontFam}
-              info={() => this.infoHandler(index)}
-              cancel={() => this.cancelHandler(index)}
-              moreInfo={this.state.fontInfo[index].showInfo}
-            />;
+            />
+          );
         })}
-        </div>
-        
       </div>
     );
   }
 }
 
 export default App;
-
